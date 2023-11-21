710f605a11c0a4f49a0406586ebacc94b3f8aa04
miche <micheknows@gmail.com>
2023-11-21 10:54:36 -0700

    added try catch so that it skips records that won't work but includes the line as a separate

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, flash
 import pandas as pd
 import io
+import os
+import secrets
 import tempfile
 import re
+import javascript
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_urlsafe()
 app.config['MIMETYPE_MAP'] = {}
 
 date_regex = re.compile(r'\d{2,4}[-/]\d{1,2}[-/]\d{1,2}')
@@ -25,6 +29,9 @@ def upload():
 
     data = []
 
+    recordfailed= False
+    failedrecords = ""
+
 
     with open(temp_path.name) as f:
         lines = f.readlines()
@@ -39,47 +46,54 @@ def upload():
         'Total': None
     }
 
+
+
     for line in lines:
         counter=counter+1
 
 
-        if date_regex.search(line):
+        try:
+            if date_regex.search(line):
 
-            # Append current row
-            if current_row and line:
-                data.append(current_row)
-                current_row = {
-                    'Date': None,
-                    'Time': None,
-                    'Flow': None,
-                    'Total': None
-                }
+                # Append current row
+                if current_row and line:
+                    recordfailed = False
+                    data.append(current_row)
+                    current_row = {
+                        'Date': None,
+                        'Time': None,
+                        'Flow': None,
+                        'Total': None
+                    }
 
 
-            # Set new date/time
-            date, time = line.split()
-            current_row['Date'] = date
-            current_row['Time'] = time
+                # Set new date/time
+                date, time = line.split()
+                current_row['Date'] = date
+                current_row['Time'] = time
 
-        elif line.startswith('FLOW'):
-            # Add to current row
-            flow = line.split(':')[1].rstrip(' g/m')
-            current_row['Flow'] = flow
+            elif line.startswith('FLOW'):
+                # Add to current row
+                flow = line.split(':')[1].rstrip(' g/m')
+                current_row['Flow'] = flow
 
-        elif line.startswith('NET'):
-            # Add to current row
-            net_regex = r'NET:\s*([+-]?\d*)x(\d*)Gal'
+            elif line.startswith('NET'):
+                # Add to current row
+                net_regex = r'NET:\s*([+-]?\d*)x(\d*)Gal'
 
-            match = re.search(net_regex, line)
-            if match:
-                mytotal = match.group(1)
-                print("original:  " + line)
-                mytotal = float(match.group(2)) / 1000 * float(match.group(1))
-                print("final:  " + str(mytotal))
-                current_row['Total'] =mytotal
-        #if(counter<20):
-            #print("counter is : " + str(counter))
-            #print(current_row)
+                match = re.search(net_regex, line)
+                if match:
+                    mytotal = match.group(1)
+                    print("original:  " + line)
+                    mytotal = float(match.group(2)) / 1000 * float(match.group(1))
+                    print("final:  " + str(mytotal))
+                    current_row['Total'] =mytotal
+            #if(counter<20):
+                #print("counter is : " + str(counter))
+                #print(current_row)
+        except Exception as ex:
+            recordfailed = True
+            failedrecords = failedrecords + "\n\n\nCounter: " + str(counter) + "\nException:  " + str(ex) + "\nRecord Failed:  " + line
 
     # Append the last row
     if current_row and line:
@@ -113,6 +127,14 @@ def upload():
 
     df.to_excel(writer, index=False)
 
+    workbook = writer.book
+    worksheet = workbook.add_worksheet("Failed Records")
+
+    # Write failed records to sheet
+    row = 0
+    for record in failedrecords.split("\n\n"):
+        worksheet.write(row, 0, record)
+        row += 1
 
 
     writer.close()
